d54f52ec61786e747c13d13a028c12c1200b4c6e
HendersonSoftworks <thomas.henderson@hendersonsoftworks.com>
2025-08-10 14:31:46 +0100

    Blocking basics done

--- Assets/Scripts/PlayerAttackController.cs
+++ Assets/Scripts/PlayerAttackController.cs
@@ -11,9 +11,11 @@ public class PlayerAttackController : MonoBehaviour
     private PlayerAnimationController animationController;
     public PlayerInput playerInput;
     public InputAction attackAction;
+    public InputAction blockAction;
 
     [Header("Logic - Do not change")]
     public bool isAttacking = false;
+    public bool isDefending = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,23 +24,53 @@ public class PlayerAttackController : MonoBehaviour
         animationController = GetComponent<PlayerAnimationController>();
 
         attackAction = playerInput.actions["Attack"];
-
+        blockAction = playerInput.actions["Block"];
     }
 
     // Update is called once per frame
     void Update()
     {
+        ManageDefending();
         ManageAttacking();
+    }
+
+    private void ManageDefending()
+    {
+        if (!blockAction.triggered) { return; }
+        if (isAttacking) { return; }
+        
+        float blockValue = blockAction.ReadValue<float>();
+        if (blockValue == 0) { FinishDefend(); ; }
+        else if (blockValue == 1) { StartDefend(); }
     }
 
     private void ManageAttacking()
     {
+        if (isDefending) { return; }
+
         if (isAttacking) { return; }
         float attackValue = attackAction.ReadValue<float>();
         if (attackValue == 0) { return; }
         
         isAttacking = true; // reset from eventhelper
         StartAttack();
+    }
+
+    public void StartDefend()
+    {
+        animationController.playerAnimator.SetBool("isDefending", true);
+        movementController.SetVelToZero();
+        movementController.canMove = false;
+        movementController.moveSpeed = movementController.attackMoveSpeed;
+        isDefending = true;
+    }
+
+    public void FinishDefend()
+    {
+        animationController.playerAnimator.SetBool("isDefending", false);
+        movementController.canMove = true;
+        movementController.moveSpeed = movementController.baseSpeed;
+        isDefending = false;
     }
 
     public void StartAttack()
